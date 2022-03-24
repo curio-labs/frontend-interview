22bb05d51f860492d7f8ec56b1516b5bf313a98c
Ivo <ivosequeros@me.com>
2022-03-24 11:13:34 +0000

    Removed line

--- pages/publications.tsx
+++ pages/publications.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import { Publication } from "../model/publication";
 
 const ENDPOINT = "https://api.curio.io/api/providers?type=publication";
 
